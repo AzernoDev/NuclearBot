4fab50b92ca270271e504a096fa73c9146acbefe
Alix BESSON <alix.besson973@gmail.com>
2020-09-28 17:10:08 +0200

    add script start

--- src/template.js
+++ src/template.js
@@ -1,24 +1,12 @@
 class Command {
 
-    #name
-    #method
-    #description;
+    name
+    method
+    description;
 
     constructor(name, method, description) {
-        this.#name = name;
-        this.#method = method;
-        this.#description = description;
-    }
-
-    get GetName() {
-        return this.#name;
-    }
-
-    get Method() {
-        this.#method();
-    }
-
-    get Describe() {
-        return this.#description;
+        this.name = name;
+        this.method = method;
+        this.description = description;
     }
 }
