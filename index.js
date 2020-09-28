68284944a10016e1a855a08a17dd589d76fd6c5f
Alix BESSON <alix.besson973@gmail.com>
2020-09-28 17:34:13 +0200

    Toumarsh

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
-}).listen(80, "127.0.0.1");
+}).listen(process.env.PORT || 80);
 
 
 const config = require('./config.json');
