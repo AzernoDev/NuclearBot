2f01a4b8e92fdddb87fb93557d48529375a25694
Alix BESSON <alix.besson973@gmail.com>
2020-09-28 17:25:34 +0200

    Add dummy web server

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Dummy web server for Heroku
+const http = require('http');
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(80, "x.x.x.x");
+
+
 const config = require('./config.json');
 
 const discord = require('discord.js');
