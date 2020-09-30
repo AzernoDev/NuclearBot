d0e8587f9be927be14bd090bec168f0aace250b4
Alix BESSON <alix.besson973@gmail.com>
2020-09-30 17:29:53 +0200

    test custom statu

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-// Dummy web server for Heroku
-const http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(process.env.PORT || 80);
-
-
 const config = require('./config.json');
+const version = "Beta";
 
 const discord = require('discord.js');
 const client = new discord.Client();
 
+
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    let logMessage = `Logged in as \`${client.user.tag}\`!`
+
+    console.log(logMessage);
+    client.channels.fetch(config.ErrorChannelID)
+        .then(channel => channel.send(logMessage));
+    client.user.setStatus('idle');
+    client.user.setActivity(
+        'Create by ' + client.users.fetch(config.AdminID.Author).name,
+        { type: 'PLAYING' })
 });
 
 client.on('message', msg => {
 
-    console.log('received message');
     //Check Prefix
-    if(!msg.content.startsWith(config.Prefix)) return;
-
-    console.log('received command');
+    if(!msg.content.startsWith(config.Prefix[version])) return;
 
     //Delete prefix
     msg.content = msg.content.slice(config.Prefix.length, msg.content.length);
 
-    console.log('command: ' + msg.content);
-
     //Basic command
     if (msg.content === 'ping') {
-        msg.reply('Pong!');
+        msg.reply('Pong!').then();
     }
+
+    //Todo: create extensible command structure
+    //Todo: create error ping on specific channel
 });
 
-client.login(config.Token);
+client.login(config.Token[version]).catch(console.error);
