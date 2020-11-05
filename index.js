0c558801fd6fd089db09488350c89476a26278b0
Azern0 <alix.besson973+work@gmail.com>
2020-11-05 20:20:12 +0100

    many change

--- index.js
+++ index.js
@@ -1,53 +1,48 @@
 const config = require('./config.json');
-const version = "Beta";
-
 const discord = require('discord.js');
 const client = new discord.Client();
 
-const commandTemplate = require('./src/template');
-let commands;
-
 client.on('ready', () => {
-    let logMessage = `Logged in as \`${client.user.tag}\`!`
+    let logMsg = `Logged in as \`${client.user.tag}\`!`
 
-    console.log(logMessage);
-    client.channels.fetch(config.LogChannelID)
-        .then(channel => channel.send(logMessage));
+    console.log(logMsg);
+    SendLog(logMsg);
 
     //TODO: Set personal status
-    client.user.setStatus('idle');
+    client.user.setStatus('idle').then(r => SendError(r));
     client.user.setActivity(
-        'Create by ' + client.users.fetch(config.AdminID.Author).name,
-        { type: 'PLAYING' })
-});
+        'Create by ' + client.users.fetch(config.AdminID).name,
+        {type: 'PLAYING'}).then(r => SendError(r))
+ });
 
 client.on('message', msg => {
 
     //Check Prefix
-    if(!msg.content.startsWith(config.Prefix[version])) return;
+    if(!msg.content.startsWith(config.Prefix)) return;
 
     //Delete prefix
-    msg.content =
-        msg.content.slice(config.Prefix[version].length, msg.content.length);
+    msg.content = msg.content.slice(config.Prefix.length, msg.content.length);
 
     let msgArray = msg.content.split(' ');
 
     //Basic command
-    if (messageArray[0] === 'ping') {
-        msg.reply('Pong!').then();
-    }
-
-    commands = {
-        'test': new commandTemplate('test', () => {
-            client.channels.fetch(config.TestChannelID)
-                .then(channel => channel.send("ceci est un test"));
-        })
+    if (msgArray[0] === 'ping') {
+        msg.reply('Pong!').then(r => SendError(r));
     }
 
     console.log(msg.content);
-    commands[msgArray[0]].method(msgArray);
     //Todo: Create extensible command structure
     //Todo: Create error ping on specific channel
 });
 
-client.login(config.Token[version]).catch(r => console.error(r));
+function SendLog(_msg)
+{
+    client.channels.fetch(config.Channel.Log).then(channel => channel.send(_msg));
+}
+
+function SendError(_msg)
+{
+    client.channels.fetch(config.Channel.Error).then(channel => channel.send(_msg));
+}
+
+client.login(config.Token).catch(r => console.error(r));
