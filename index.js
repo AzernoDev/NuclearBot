7e060c97174f89ab0edf69f789cb81adf2e48391
Azern0 <alix.besson973+work@gmail.com>
2020-11-07 13:12:42 +0100

    many change

--- index.js
+++ index.js
@@ -1,48 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
 const config = require('./config.json');
-const discord = require('discord.js');
-const client = new discord.Client();
 
 client.on('ready', () => {
-    let logMsg = `Logged in as \`${client.user.tag}\`!`
+    let logMsg = `Logged in as ${client.user.tag}!`;
 
     console.log(logMsg);
-    SendLog(logMsg);
-
-    //TODO: Set personal status
-    client.user.setStatus('idle').then(r => SendError(r));
-    client.user.setActivity(
-        'Create by ' + client.users.fetch(config.AdminID).name,
-        {type: 'PLAYING'}).then(r => SendError(r))
- });
-
-client.on('message', msg => {
-
-    //Check Prefix
-    if(!msg.content.startsWith(config.Prefix)) return;
-
-    //Delete prefix
-    msg.content = msg.content.slice(config.Prefix.length, msg.content.length);
-
-    let msgArray = msg.content.split(' ');
-
-    //Basic command
-    if (msgArray[0] === 'ping') {
-        msg.reply('Pong!').then(r => SendError(r));
-    }
-
-    console.log(msg.content);
-    //Todo: Create extensible command structure
-    //Todo: Create error ping on specific channel
 });
 
-function SendLog(_msg)
-{
-    client.channels.fetch(config.Channel.Log).then(channel => channel.send(_msg));
-}
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!').then(r => console.log(r));
+    }
+});
 
-function SendError(_msg)
-{
-    client.channels.fetch(config.Channel.Error).then(channel => channel.send(_msg));
-}
-
-client.login(config.Token).then(r => console.error(r));
+client.login(config.Token).catch(r => console.log(r));
