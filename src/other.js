ad56923bb9f35033cc215c830e824b256b46a103
AzernoDev <alix.besson973@gmail.com>
2020-11-22 20:31:45 +0100

    My friend ask me every day for add this shitting command xD

--- src/other.js
+++ src/other.js
@@ -13,4 +13,19 @@ module.exports = [
         } else {
             msg.reply('pong');
         }
-    })]
+    }),
+
+    new command("tic", "My friend spam me for add this shitting command xD", function (client, args, msg) {
+
+        if(args.length > 0) {
+            const arg = args.shift();
+
+            if(arg === "type")
+            {
+                msg.channel.send(`Command type : ${this.type}`)
+            }
+        } else {
+            msg.reply('tac');
+        }
+    })
+]
