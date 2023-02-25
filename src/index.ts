3935f0721c89df78bb2391698816dda8c2d8d7a5
AzernoDev <alix.besson.dev@gmail.com>
2023-02-25 17:34:16 +0100

    Add linter add editorconfig file

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import {Client, Events, GatewayIntentBits} from "discord.js";
-import {validateEnv} from "./utils/validator/validateEnv";
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import { validateEnv } from "./utils/validator/validateEnv";
 
 (async () => {
-    if (!validateEnv()) return;
-    const client = new Client({intents: [GatewayIntentBits.Guilds]});
+	if(!validateEnv()) return;
+	const client = new Client({ intents: [ GatewayIntentBits.Guilds ] });
 
-    client.once(Events.ClientReady, _client => {
-        console.log(`Ready! Logged in as ${_client.user.tag}`);
-    });
+	client.once(Events.ClientReady, _client => {
+		console.log(`Ready! Logged in as ${_client.user.tag}`);
+	});
 
-    await client.login(process.env.CLIENT_TOKEN);
+	await client.login(process.env.CLIENT_TOKEN);
 })();
